9113b99976d0c22a30a5f2978644677d593468d9
MaGo1024 <34481013+MaGo1024@users.noreply.github.com>
2020-04-12 02:38:37 +0100

    Second Completion

--- src/estimator.js
+++ src/estimator.js
@@ -1,18 +1,4 @@
-data = {
-  region: {
-    name: "Africa",
-    avgAge: 19.7,
-    avgDailyIncomeInUSD: 5,
-    avgDailyIncomePopulation: 0.71
-  },
-  periodType: "days",
-  timeToElapse: 58,
-  reportedCases: 674,
-  population: 66622705,
-  totalHospitalBeds: 1380614
-};
-
-const covid19ImpactEstimator = (data) => {
+ const covid19ImpactEstimator = (data) => {
   const currentlyInfected = (rate) => (data.reportedCases * rate);
   const impact = {
     currentlyInfected: Math.round(currentlyInfected(10)),
@@ -21,9 +7,8 @@ const covid19ImpactEstimator = (data) => {
     hospitalBedsByRequestedTime: Math.round((data.totalHospitalBeds * 0.35) - (currentlyInfected(10) * 512 * 0.15)),
     casesForICUByRequestedTime: Math.round(currentlyInfected(10) * 512 * 0.05),
     casesForVentilatorsByRequestedTime: Math.round(currentlyInfected(10) * 512 * 0.02),
-    dollarsInFlight: (currentlyInfected(10) * 512 * 0.65 * 1.5 * 30).toFixed(2)
+    dollarsInFlight: currentlyInfected(10) * 512 * 0.65 * 1.5 * 30
   };
-
   const severeImpact = {
     currentlyInfected: Math.round(currentlyInfected(50)),
     infectionsByRequestedTime: Math.round(currentlyInfected(50) * 512),
@@ -31,11 +16,9 @@ const covid19ImpactEstimator = (data) => {
     hospitalBedsByRequestedTime: Math.round((data.totalHospitalBeds * 0.35) - (currentlyInfected(50) * 512 * 0.15)),
     casesForICUByRequestedTime: Math.round(currentlyInfected(10) * 512 * 0.05),
     casesForVentilatorsByRequestedTime: Math.round(currentlyInfected(10) * 512 * 0.02),
-    dollarsInFlight: (currentlyInfected(10) * 512 * 0.65 * 1.5 * 30).toFixed(2)
+    dollarsInFlight: currentlyInfected(50) * 512 * 0.65 * 1.5 * 30
   };
-
-  return {data, impact, severeImpact}
-
+  const estimate = { impact, severeImpact };
+  return { data, estimate };
 };
-
 export default covid19ImpactEstimator;
